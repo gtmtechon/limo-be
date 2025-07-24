8405c80f834c1125583a5f526bbf69d58d0cc033
gtmtech <ux20me@gmail.com>
2025-07-24 19:59:42 +0900

    dev env modified

--- src/main/java/com/handson/gtmtech/limo/be/config/WebConfig.java
+++ src/main/java/com/handson/gtmtech/limo/be/config/WebConfig.java
@@ -10,7 +10,9 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 모든 경로에 대해 CORS 허용
-                .allowedOrigins("http://localhost:8081", "limoapi-bkd5e9huhja0defa.koreacentral-01.azurewebsites.net","http://localhost:8080") // Vue.js 앱의 Origin 허용
+                .allowedOrigins("http://localhost:8081", 
+                "limoapi-bkd5e9huhja0defa.koreacentral-01.azurewebsites.net","http://localhost:8080",
+                "https://limoweb.azurewebsites.net") // Vue.js 앱의 Origin 허용
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용할 HTTP 메서드
                 .allowedHeaders("*") // 모든 헤더 허용
                 .allowCredentials(true); // 자격 증명(쿠키, 인증 헤더 등) 허용
